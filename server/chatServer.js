dafd173463bcf3a94a22562a44705cbf4c4cbed4
Leonardo Dias de Vasconcelos <leodias.vasconcelos@yahoo.com.br>
2014-08-14 15:55:44 -0300

    Create Web Socket Server, after downloading with npm, even with "native code compile failed!".

--- server/chatServer.js
+++ server/chatServer.js
@@ -4,3 +4,6 @@ var server = http.createServer(function(request, response){});
 server.listen(8080, function(){
 	console.log((new Date()) + ' Server is listening on port 8080');
 });
+
+var WebSocketServer = require('websocket').server;
+var wsServer = new WebSocketServer({ httpServer: server });
